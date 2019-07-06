61e4622fd6f3111fbed9be506ea0741a34251322
Sasindu umesh <45946252+coolsasindu@users.noreply.github.com>
2019-07-07 01:27:38 +0530

    Add files via upload

---
+++ Table_cal/src/table_cal/Table_cal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package table_cal;
+
+/**
+ *
+ * @author Go
+ */
+public class Table_cal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
